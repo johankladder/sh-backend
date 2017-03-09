80eb4910582776c77dd3575f9e369048de94735c
Johan Kladder <j.kladder@st.hanze.nl>
2017-03-09 19:04:30 +0100

    + Added heroku support

--- src/main/java/RestApi.java
+++ src/main/java/RestApi.java
@@ -6,9 +6,17 @@ import static spark.Spark.*;
 public class RestApi {
 
     public static void main(String[] args) {
-
+        port(getHerokuAssignedPort());
         get("/hello", (req, res) -> "Hello Hoppers!");
 
+    }
+
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
 }
